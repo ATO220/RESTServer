b58ae14ab69c1e6757232c99a552dfa8e4357d82
ATO220 <serranolaureano.220@gmail.com>
2018-12-18 13:28:23 -0300

    new change

--- database.js
+++ database.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/db-rest-server';
+const URI = process.env.DB_URI || 'mongodb://localhost:27017/db-rest-server';
+const DB_USER =  process.env.DB_USER || "";
+const DB_PASS = process.env.DB_PASS || "";
+const options = {
+  user : DB_USER,
+  pass : DB_PASS,
+  useNewUrlParser: true
+};
 
-mongoose.connect(URI)
+
+mongoose.connect(URI, options)
     .then(db => console.log('DB is connect'))
     .catch(err => console.error(err));
 
